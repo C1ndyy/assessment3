a4bddaee07bc506abdbc72ceb0d92a2c49dca3e9
C1ndyy <cindx.xu@gmail.com>
2021-04-09 14:36:10 -0600

    complete assessment 3

---
+++ widget/main_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Widget
+from .forms import WidgetForm
+from django.db.models import Sum
+
+# Create your views here.
+def add_widget(request):
+    form = WidgetForm(request.POST)
+    if form.is_valid():
+        new_widget = form.save(commit=False)
+        new_widget.save()
+    return redirect('home')
+  
+
+def home(request):
+    widget_list = Widget.objects.all()
+    widget_form = WidgetForm()
+    total = Widget.objects.aggregate(Sum('quantity'))
+    return render(request, 'home.html' , {'widget_list': widget_list, 'widget_form': widget_form, 'total': total})
+
+def delete_widget(request, widget_id):
+    Widget.objects.get(id=widget_id).delete()
+    return redirect('home')
